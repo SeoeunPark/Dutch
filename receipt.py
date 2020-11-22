a9d8ddb723e9d4f8b4dd0e02b3ee117e919b619c
SeoeunPark <s2019w29@e-mirim.hs.kr>
2020-11-22 17:47:51 +0900

    menu_insert.py에서 receipt.py 로 값 전달 기능 추가

--- receipt.py
+++ receipt.py
@@ -7,26 +7,29 @@ from tkinter import *
 
 
 class Receipt:
-    def __init__(self, receipt):
+    def __init__(self, receipt, inputmenu):
+        self.inputm = inputmenu
+        self.one = self.inputm[0]
+
 
         fontq = tkinter.font.Font(size=13, weight='bold')
-        fontm = tkinter.font.Font(size=17, weight='bold')
+        fontm = tkinter.font.Font(size=10, weight='bold')
         self.receipt = receipt
 
-        self.receiptBack = tkinter.PhotoImage(file="image/eee.gif")
+        self.receiptBack = tkinter.PhotoImage(file="image/white.png")
         self.receiptBackL = tkinter.Label(image=self.receiptBack)
         self.receiptBackL.place(x=-2, y=-2)
 
-        self.locationText = Label(self.receipt, text='더치페이', width=10, fg='#ff7878', font=fontm, bg='#d8f4ff')
+        self.locationText = Label(self.receipt, text=self.inputm,fg='#ff7878', font=fontm, bg='white')
         self.locationText.place(x=400, y=10)
 
         self.backButton = Button(self.receipt, width=4, text='⇦', repeatdelay=20, bg='#ff7878', font=fontq,
-                                 fg="white",command=self.back)  # command=self.to_receipt,
+                                 fg="white", command=self.back)  # command=self.to_receipt,
         self.backButton.place(x=20, y=25)
 
-        self.saveButton = Button(self.receipt, width=30, text='저 장', repeatdelay=20,bg='#ff7878', font=fontq, fg="white")  # command=self.to_receipt,
+        self.saveButton = Button(self.receipt, width=30, text='저 장', repeatdelay=20, bg='#ff7878', font=fontq,
+                                 fg="white")  # command=self.to_receipt,
         self.saveButton.place(x=300, y=650)
-
 
     def green(self):
         pass
@@ -41,14 +44,14 @@ class Receipt:
         self.receipt.mainloop()
 
     def back(self):
-        Move= menu_insert.Menuinsert(self.receipt)
+        Move = menu_insert.Menuinsert(self.receipt)
 
 
-if __name__=='__main__':
-    menu = tkinter.Tk()
-    menu.title("더치 페이")
-    menu.geometry("900x700+100+100")
-    menu.resizable(False, False)
-
-    re = Receipt(menu)
-    re.play()
+# if __name__ == '__main__':
+#     menu = tkinter.Tk()
+#     menu.title("더치 페이")
+#     menu.geometry("900x700+100+100")
+#     menu.resizable(False, False)
+#
+#     re = Receipt(menu)
+#     re.play()
